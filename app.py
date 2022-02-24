ffe1355d352ee74855161eec66c8ef7f33d9a17c
santiagorestrepoo09 <dsrestrepo04@ucatolica.edu.co>
2022-02-24 11:25:24 -0500

    Se crea la realizan cambios para la consula sql

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import psycopg2
 from psycopg2 import Error
 from jinja2 import Environment, FileSystemLoader
 import os
+from util.correo import Correo
 
 t_host = "192.168.102.29" 
 t_port = "5432"
@@ -13,7 +14,7 @@ db_conn = psycopg2.connect(host=t_host, port=t_port, dbname=t_name_db, user=t_us
 db_cursor = db_conn.cursor()
 
 def Consulta_arrayClientes():
-  Sql = " SELECT cl.cliente_codigo,car.empresa_codigo , te.empresa_nombre  from tb_factura tf left join tb_cartera car on (car.factura_codigo = tf.factura_codigo) left join tb_carteracambio tc on (tc.cartera_codigo = car.cartera_codigo) left join tb_centrocosto tc2 on (tc2.cencos_codigo = tf.cencos_codigo) left join tb_cliente cl on 	(cl.cliente_codigo = car.cliente_codigo) left join tb_empresa te on	(te.empresa_codigo = tf.empresa_codigo) where  car.cartera_saldo >= 1  and car.empresa_codigo = 1 group by cl.cliente_codigo,car.empresa_codigo,te.empresa_nombre  "
+  Sql = " SELECT cl.cliente_codigo,car.empresa_codigo , te.empresa_nombre  from tb_factura tf left join tb_cartera car on (car.factura_codigo = tf.factura_codigo) left join tb_carteracambio tc on (tc.cartera_codigo = car.cartera_codigo) left join tb_centrocosto tc2 on (tc2.cencos_codigo = tf.cencos_codigo) left join tb_cliente cl on 	(cl.cliente_codigo = car.cliente_codigo) left join tb_empresa te on	(te.empresa_codigo = tf.empresa_codigo) where  car.cartera_saldo >= 1	and car.empresa_codigo in (1,2,12) group by cl.cliente_codigo,car.empresa_codigo,te.empresa_nombre limit 3 "
   ArrayClientes = []
   ArrayEmpresaNombre = []
   ArrayEmpresaCodigo = []
@@ -59,7 +60,14 @@ def Consulta_arrayClientes():
         }
         RutaPDf = path_abs +"/pdfs/"
         ruta = (f'{RutaPDf}{cliente_codigo}{"_"}{ArrayEmpresaCodigo[im]}{".pdf"}')
+        imagen = '<img src="../img/'+str(ArrayEmpresaNombre[im])+'.png"/>'
+        print(imagen)
         pdfkit.from_file(RutaAbrir , ruta, options=options)
+        ##recipients = ["david.restrepo@mct.com.co","deysi.orjuela@mct.com.co"]
+        recipients = ["david.restrepo@mct.com.co"]
+        title = "Informe Facturas pendientes"
+        print(ruta)
+        Correo().send_mail(ruta, recipients, title,imagen)
   except psycopg2.Error as error:
     print("Error en la connecting a PostgreSQL", error)
   db_cursor.close()
@@ -89,7 +97,7 @@ def Consultar_DatosClientes(List_Clientes,empresa_codigo):
 
 def Consultar_FaturasClientes(List_Clientes,empresa_codigo):
 
-  StringsqlCliente = " SELECT	LPAD(emp.empresa_codigocontable, 2, '0')|| LPAD(cc.cencos_digito, 3, '0')|| '-' || coalesce(f.factura_numerodocumento, f.factura_cencoscodigo::text) as prefijo ,  substr(f.factura_fechacreacion::text,	1,	10)   as fecha_creacion, substr((c.cartera_fechacreacion::date + f.factura_diasvencidos::int)::text,	1,	10)   as fecha_vencimiento,  	fc.factura_total as valorfactura,	now()::date-c.cartera_fechacreacion-cl.cliente_diasvencimientofactura as dias_vencidos  from tb_factura f left join tb_facturacambio fc on (f.factura_codigo = fc.factura_codigo and fc.moneda_codigo = 1) left join tb_cartera c on f.factura_codigo = c.factura_codigo left join tb_carteracambio ccam on c.cartera_codigo = ccam.cartera_codigo and ccam.moneda_codigo = 1 left join tb_itemcarteraprod icp on f.factura_codigo = icp.factura_codigo	and coalesce( icp.itemcarteraprod_fechadocumento, icp.itemcarteraprod_fechacreacion::date) <= current_date::date and (icp.itemcarteraprod_anulado = 'NO' or (icp.itemcarteraprod_anulado = 'SI'	and icp.itemcarteraprod_fechaanulacion::date >= current_date::date)) left join tb_itemcarteraprodcambio icpc on f.factura_codigo = icpc.factura_codigo and icpc.moneda_codigo = 1 and icp.itemcarteraprod_codigo = icpc.itemcarteraprod_codigo and icp.itemcarteraprod_subitem = icpc.itemcarteraprod_subitem inner join tb_cliente cl on f.cliente_codigo = cl.cliente_codigo inner join tb_centrocosto cc on (f.cencos_codigo = cc.cencos_codigo) inner join tb_empresa emp on (emp.empresa_codigo = cc.empresa_codigo) inner join tb_estado e on	f.estado_codigo = e.estado_codigo left join tb_vendedor v on f.vendedor_codigo = v.vendedor_codigo left join tb_ciudad cd on f.ciudad_codigo_pago = cd.ciudad_codigo left join tb_ingresocaja ti on icp.documento_codigo = 9 and ti.ingca_codigo = icp.itemcarteraprod_numero left join tb_comprobanteingreso tc on icp.documento_codigo = 31 and tc.coming_codigo = icp.itemcarteraprod_numero and(tc.fecha_anulacion is null or tc.fecha_anulacion::date > current_date ) left join tb_notacontabilidad tn on icp.documento_codigo = 15  and tn.notcon_codigo = icp.itemcarteraprod_numero and (tn.notcon_fecha_anulacion is null or tn.notcon_fecha_anulacion::date > current_date) where f.factura_fechacreacion::date <= current_date and f.estado_codigo in (70, 71, 78, 69, 146) and cl.cliente_codigo =  "
+  StringsqlCliente = " SELECT	'E'|| LPAD(cc.cencos_digito, 3, '0')|| '-' || coalesce(f.factura_numerodocumento, f.factura_cencoscodigo::text) as prefijo ,  substr(f.factura_fechacreacion::text,	1,	10)   as fecha_creacion, substr((c.cartera_fechacreacion::date + f.factura_diasvencidos::int)::text,	1,	10)   as fecha_vencimiento,  	fc.factura_total as valorfactura,	now()::date-c.cartera_fechacreacion-cl.cliente_diasvencimientofactura as dias_vencidos  from tb_factura f left join tb_facturacambio fc on (f.factura_codigo = fc.factura_codigo and fc.moneda_codigo = 1) left join tb_cartera c on f.factura_codigo = c.factura_codigo left join tb_carteracambio ccam on c.cartera_codigo = ccam.cartera_codigo and ccam.moneda_codigo = 1 left join tb_itemcarteraprod icp on f.factura_codigo = icp.factura_codigo	and coalesce( icp.itemcarteraprod_fechadocumento, icp.itemcarteraprod_fechacreacion::date) <= current_date::date and (icp.itemcarteraprod_anulado = 'NO' or (icp.itemcarteraprod_anulado = 'SI'	and icp.itemcarteraprod_fechaanulacion::date >= current_date::date)) left join tb_itemcarteraprodcambio icpc on f.factura_codigo = icpc.factura_codigo and icpc.moneda_codigo = 1 and icp.itemcarteraprod_codigo = icpc.itemcarteraprod_codigo and icp.itemcarteraprod_subitem = icpc.itemcarteraprod_subitem inner join tb_cliente cl on f.cliente_codigo = cl.cliente_codigo inner join tb_centrocosto cc on (f.cencos_codigo = cc.cencos_codigo) inner join tb_empresa emp on (emp.empresa_codigo = cc.empresa_codigo) inner join tb_estado e on	f.estado_codigo = e.estado_codigo left join tb_vendedor v on f.vendedor_codigo = v.vendedor_codigo left join tb_ciudad cd on f.ciudad_codigo_pago = cd.ciudad_codigo left join tb_ingresocaja ti on icp.documento_codigo = 9 and ti.ingca_codigo = icp.itemcarteraprod_numero left join tb_comprobanteingreso tc on icp.documento_codigo = 31 and tc.coming_codigo = icp.itemcarteraprod_numero and(tc.fecha_anulacion is null or tc.fecha_anulacion::date > current_date ) left join tb_notacontabilidad tn on icp.documento_codigo = 15  and tn.notcon_codigo = icp.itemcarteraprod_numero and (tn.notcon_fecha_anulacion is null or tn.notcon_fecha_anulacion::date > current_date) where f.factura_fechacreacion::date <= current_date and f.estado_codigo in (70, 71, 78, 69, 146) and cl.cliente_codigo =  "
   Stringsqlcliente2 = " group by	f.cencos_codigo,	emp.empresa_codigocontable,	cc.cencos_digito,	f.cliente_codigo,	c.cartera_fechacreacion,	f.factura_codigo,	f.factura_cencoscodigo,	f.factura_cencoscodigo,	f.vendedor_codigo,	fc.factura_total,	f.factura_diasvencidos,	ccam.cambio_trm ,	f.factura_fechacreacion::date,	f.estado_codigo,	ccam.cartera_debito,	ccam.cartera_credito,	f.ciudad_codigo_pago,	cl.cliente_diasvencimientofactura,	v.vendedor_nombre,	f.factura_fechacreacion::date having 	(coalesce(ccam.cartera_credito, 0) - SUM((coalesce(icp.itemcarteraprod_multiplicador, 0) * coalesce(icpc.cambio_trm, 0) * coalesce(case when icp.itemcarteraprod_anulado <> 'SI' and coalesce ( ti.ingca_fechacreacion::date , tc.coming_fecha::date, tn.notcon_fechacreacion::date) <= current_date::date then icpc.itemcarteraprod_valor else 0 end, 0) )) ) >1 order by	dias_vencidos desc"
   postgreSQL_cliente = ( f' {StringsqlCliente}{List_Clientes}{" and c.empresa_codigo =  "}{empresa_codigo}{Stringsqlcliente2} ' )
   try:

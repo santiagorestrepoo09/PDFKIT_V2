ef947bbb8391342c71f96b62f360b7cf991031d6
santiagorestrepoo09 <dsrestrepo04@ucatolica.edu.co>
2022-03-02 08:00:42 -0500

    Se agrega cambio de recursividad quitar sql donde trai la suma de las facturas

--- app.py
+++ app.py
@@ -14,8 +14,10 @@ db_conn = psycopg2.connect(host=t_host, port=t_port, dbname=t_name_db, user=t_us
 db_cursor = db_conn.cursor()
 
 def Consulta_arrayClientes():
-  Sql = " SELECT cl.cliente_codigo,car.empresa_codigo , te.empresa_nombre,empresa_documento, 	cl.cliente_emailfacturavencer  from tb_factura tf left join tb_cartera car on (car.factura_codigo = tf.factura_codigo) left join tb_carteracambio tc on (tc.cartera_codigo = car.cartera_codigo) left join tb_centrocosto tc2 on (tc2.cencos_codigo = tf.cencos_codigo) left join tb_cliente cl on 	(cl.cliente_codigo = car.cliente_codigo) left join tb_empresa te on	(te.empresa_codigo = tf.empresa_codigo)  where  car.cartera_saldo >= 1	and car.empresa_codigo in (1,2,12) and cl.cliente_excluir is true and car.cliente_codigo not in (1294,2524,140,2412,1301,625,2329,2372,1848,2739,2484,2493,1929,1993,1369,974,2030,1660,1660,7,2463,2730,2543,2466,2616)  group by cl.cliente_codigo,car.empresa_codigo,te.empresa_nombre,empresa_documento  "
+  Sql = " SELECT cl.cliente_codigo, 	cl.cliente_nombre1, substr(now()::text,	1,10) as fechacorte, car.empresa_codigo , te.empresa_nombre,empresa_documento, 	cl.cliente_emailfacturavencer  from tb_factura tf left join tb_cartera car on (car.factura_codigo = tf.factura_codigo) left join tb_carteracambio tc on (tc.cartera_codigo = car.cartera_codigo) left join tb_centrocosto tc2 on (tc2.cencos_codigo = tf.cencos_codigo) left join tb_cliente cl on 	(cl.cliente_codigo = car.cliente_codigo) left join tb_empresa te on	(te.empresa_codigo = tf.empresa_codigo)  where  car.cartera_saldo >= 1	and car.empresa_codigo in (1,2,12) and cl.cliente_excluir is true and car.cliente_codigo   in (1848)  group by cl.cliente_codigo,car.empresa_codigo,te.empresa_nombre,empresa_documento  "
   ArrayClientes = []
+  ArrayclienteNombre = []
+  ArrayFechacorte = []
   ArrayEmpresaNombre = []
   ArrayEmpresaCodigo = []
   ArrayEmpresaDocumento = []
@@ -29,19 +31,32 @@ def Consulta_arrayClientes():
       for i in range(len(List_Clientes)):
         for j in range(0,1):
           ArrayClientes.append(List_Clientes[i][j])
-          ArrayEmpresaCodigo.append(List_Clientes[i][j+1])
-          ArrayEmpresaNombre.append(List_Clientes[i][j+2])
-          ArrayEmpresaDocumento.append(List_Clientes[i][j+3])
-          ArrayClienteEmailfacturavencer.append(List_Clientes[i][j+4])
-      #print(ArrayClientes)
-      #print(ArrayEmpresaCodigo)
+          ArrayclienteNombre.append(List_Clientes[i][j+1])
+          ArrayFechacorte.append(List_Clientes[i][j+2])
+          ArrayEmpresaCodigo.append(List_Clientes[i][j+3])
+          ArrayEmpresaNombre.append(List_Clientes[i][j+4])
+          ArrayEmpresaDocumento.append(List_Clientes[i][j+5])
+          ArrayClienteEmailfacturavencer.append(List_Clientes[i][j+6])
+      ##print(ArrayclienteNombre)
+      ##print(ArrayFechacorte)
       ##print(ArrayEmpresaNombre)
       for im in range(len(ArrayClientes)):
         cliente_codigo = ArrayClientes[im]
         empresa_codigo = ArrayEmpresaCodigo[im]
-        DatosClientes = Consultar_DatosClientes(cliente_codigo,empresa_codigo)
+        ArrayDatosClientes = {
+              'nombreempresa':ArrayEmpresaNombre[im],   
+              'cliente_nombre1':ArrayclienteNombre[im],
+              'fechacorte':ArrayFechacorte[im], 
+        }
+        DatosClientes = ArrayDatosClientes
         DatosClientes['empresa_imagen'] = '<img src="../img/'+str(ArrayEmpresaNombre[im])+'.png"/>'
-        DatosClientes['facturas'] = Consultar_FaturasClientes(cliente_codigo,empresa_codigo)
+        ArrayFacturas = Consultar_FaturasClientes(cliente_codigo,empresa_codigo)
+        ##print(ArrayFacturas)
+        
+        DatosClientes['totalestadocuenta'] = ArrayFacturas[0]['Total_cuenta']
+        DatosClientes['valorfacturavencidas'] = ArrayFacturas[0]['Saldo_diasvencidos']
+        DatosClientes['valorfacturasinvencer'] = ArrayFacturas[0]['Saldo_diasSinvencer']
+        DatosClientes['facturas'] = ArrayFacturas
         BancosString = Consultar_BancosEmpresa(empresa_codigo)
         path_abs = os.path.dirname(__file__)
         env = Environment(loader=FileSystemLoader(path_abs +"/template"))
@@ -56,7 +71,7 @@ def Consulta_arrayClientes():
             'orientation': 'Portrait',
             'margin-top': '0.01in',
             'margin-right': '0.50in',
-            'margin-bottom': '0.01in',
+            'margin-bottom': '0.01in',  
             'margin-left': '0.50in',
             'encoding': "UTF-8",
         }
@@ -82,28 +97,6 @@ def Consulta_arrayClientes():
   db_cursor.close()
   db_conn.close()
 
-def Consultar_DatosClientes(List_Clientes,empresa_codigo):
-  Sql2 = (f'{"SELECT te.empresa_nombre as nombreEmpresa,	cl.cliente_nombre1, substr(now()::text,	1,	10) as fechacorte,	sum(car.cartera_saldo) as TotalEstadoCuenta,	(	select	case	when sum(car.cartera_saldo) is null then 0 else sum(car.cartera_saldo) end as ValorFacturaVencidas from tb_factura ft	left join tb_cartera car on (car.factura_codigo = ft.factura_codigo) left join tb_carteracambio tc on (tc.cartera_codigo = car.cartera_codigo) left join tb_cliente cl on (cl.cliente_codigo = car.cliente_codigo) left join tb_facturacambio fc on (ft.factura_codigo = fc.factura_codigo) where cl.cliente_codigo = "}{List_Clientes}{" 	and car.empresa_codigo ="}{empresa_codigo}{"	and now()::date-car.cartera_fechacreacion-cl.cliente_diasvencimientofactura > 0 and tc.cartera_saldo >=1), (select case when sum(car.cartera_saldo) is null then 0 else sum(car.cartera_saldo) end as ValorFacturaSinVencer from tb_factura ft left join tb_cartera car on (car.factura_codigo = ft.factura_codigo) left join tb_carteracambio tc on (tc.cartera_codigo = car.cartera_codigo) left join tb_cliente cl on (cl.cliente_codigo = car.cliente_codigo) left join tb_facturacambio fc on (ft.factura_codigo = fc.factura_codigo) where cl.cliente_codigo = "}{List_Clientes}{" and car.empresa_codigo ="}{empresa_codigo}{"and now()::date-car.cartera_fechacreacion-cl.cliente_diasvencimientofactura <= 0 and tc.cartera_saldo >=1) from tb_factura tf left join tb_cartera car on (car.factura_codigo = tf.factura_codigo) left join tb_cliente cl on (cl.cliente_codigo = car.cliente_codigo) left join tb_carteracambio tc on (tc.cartera_codigo = car.cartera_codigo) left join tb_empresa te on (te.empresa_codigo = tf.empresa_codigo) left join tb_facturacambio fc on (tf.factura_codigo = fc.factura_codigo) where car.cliente_codigo = "}{List_Clientes}{" and car.empresa_codigo ="}{empresa_codigo}{"and car.cartera_saldo >= 1 group by te.empresa_nombre, cl.cliente_nombre1"} ')
-  try:
-      db_cursor.execute(Sql2)
-      row = db_cursor.fetchone()
-      while row is not None:
-          rowarray_list = {
-              'nombreempresa':row[0],   
-              'cliente_nombre1':row[1],
-              'fechacorte':row[2], 
-              'totalestadocuenta':("{:,.2f}".format(row[3])), 
-              'valorfacturavencidas':("{:,.2f}".format(row[4])),
-              'valorfacturasinvencer':("{:,.2f}".format(row[5])), 
-          }
-          row = db_cursor.fetchone()
-      return(rowarray_list)
-  except (Exception, Error) as error:
-    print("Error en la connections  a PostgreSQL !!!!", error)
-  db_cursor.close()
-  db_conn.close()
-
-
 def Consultar_FaturasClientes(List_Clientes,empresa_codigo):
 
   StringsqlCliente = " SELECT	'E'|| LPAD(cc.cencos_digito, 3, '0')|| '-' || coalesce(f.factura_numerodocumento, f.factura_cencoscodigo::text) as prefijo ,  substr(f.factura_fechacreacion::text,	1,	10)   as fecha_creacion, substr((c.cartera_fechacreacion::date + f.factura_diasvencidos::int)::text,	1,	10)   as fecha_vencimiento,  	c.cartera_saldo as valorfactura,	now()::date-c.cartera_fechacreacion-cl.cliente_diasvencimientofactura as dias_vencidos  from tb_factura f left join tb_facturacambio fc on (f.factura_codigo = fc.factura_codigo and fc.moneda_codigo = 1) left join tb_cartera c on f.factura_codigo = c.factura_codigo left join tb_carteracambio ccam on c.cartera_codigo = ccam.cartera_codigo and ccam.moneda_codigo = 1 left join tb_itemcarteraprod icp on f.factura_codigo = icp.factura_codigo	and coalesce( icp.itemcarteraprod_fechadocumento, icp.itemcarteraprod_fechacreacion::date) <= current_date::date and (icp.itemcarteraprod_anulado = 'NO' or (icp.itemcarteraprod_anulado = 'SI'	and icp.itemcarteraprod_fechaanulacion::date >= current_date::date)) left join tb_itemcarteraprodcambio icpc on f.factura_codigo = icpc.factura_codigo and icpc.moneda_codigo = 1 and icp.itemcarteraprod_codigo = icpc.itemcarteraprod_codigo and icp.itemcarteraprod_subitem = icpc.itemcarteraprod_subitem inner join tb_cliente cl on f.cliente_codigo = cl.cliente_codigo inner join tb_centrocosto cc on (f.cencos_codigo = cc.cencos_codigo) inner join tb_empresa emp on (emp.empresa_codigo = cc.empresa_codigo) inner join tb_estado e on	f.estado_codigo = e.estado_codigo left join tb_vendedor v on f.vendedor_codigo = v.vendedor_codigo left join tb_ciudad cd on f.ciudad_codigo_pago = cd.ciudad_codigo left join tb_ingresocaja ti on icp.documento_codigo = 9 and ti.ingca_codigo = icp.itemcarteraprod_numero left join tb_comprobanteingreso tc on icp.documento_codigo = 31 and tc.coming_codigo = icp.itemcarteraprod_numero and(tc.fecha_anulacion is null or tc.fecha_anulacion::date > current_date ) left join tb_notacontabilidad tn on icp.documento_codigo = 15  and tn.notcon_codigo = icp.itemcarteraprod_numero and (tn.notcon_fecha_anulacion is null or tn.notcon_fecha_anulacion::date > current_date) where f.factura_fechacreacion::date <= current_date and f.estado_codigo in (70, 71, 78, 69, 146) and cl.cliente_codigo =  "
@@ -115,7 +108,15 @@ def Consultar_FaturasClientes(List_Clientes,empresa_codigo):
     row = cursor.fetchone()
     header = [i[0] for i in cursor.description]
     rowarray_list  = []
+    Total_cuenta = 0
+    Saldo_diasvencidos = 0
+    Saldo_diasSinvencer = 0
     while row is not None:
+        Total_cuenta = Total_cuenta + row[3]
+        if row[4] > 0 :
+          Saldo_diasvencidos =Saldo_diasvencidos + row[3]
+        elif row[4] <= 0:
+          Saldo_diasSinvencer = Saldo_diasSinvencer + row[3]
         t = { 
             header[0]:row[0], 
             header[1]:row[1], 
@@ -125,7 +126,12 @@ def Consultar_FaturasClientes(List_Clientes,empresa_codigo):
         }
         rowarray_list.append(t)
         row = cursor.fetchone()
-    return(rowarray_list)
+    w = {
+      'Total_cuenta' : ("{:,.2f}".format(Total_cuenta)),
+      'Saldo_diasvencidos': ("{:,.2f}".format(Saldo_diasvencidos)),
+      'Saldo_diasSinvencer': ("{:,.2f}".format(Saldo_diasSinvencer)),
+    }
+    return(w,rowarray_list)
   except (Exception, Error) as error:
       print("Error while connecting to PostgreSQL", error)
   db_cursor.close()
